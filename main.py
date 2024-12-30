1fef783b615f3e9671a863946c1f5c3b1f799e98
bryanho0470 <bryanho0470@gmail.com>
2024-12-30 16:37:47 +0900

    streamlit
    install and import
    basic usages

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-import tiktoken
+import streamlit as st
+from langchain.prompts import PromptTemplate
 
+st.title("hello world!")
+
+st.subheader("Welcome to Streamlit!!")
+
+st.markdown(
+    """
+    #### I love it!
+    """
+)
+
+st.write("hi")
+
+st.write([1,2,3,4])
+
+st.write(PromptTemplate)
+
+p = PromptTemplate.from_template("xxx")
+
+st.write(p)
+
+st.selectbox(
+    label="Choose you model",
+    options=("GPT-3","GPT-4"),
+    placeholder="Please select"
+    )
