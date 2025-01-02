e33c91df2abd793d33a60e9272bc94a668364d2e
bryanho0470 <bryanho0470@gmail.com>
2025-01-02 11:21:51 +0900

    update lots of components and combine notebook and streamlit.
    - there were conflict of version. especially, pydantic. refer requirements.txt but still works.
    
    - upload file
    - split file
    - embedding
    - caching
    - return retriever
    - show resultss of retriever.invoke

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,4 @@
+from curses import qiflush
 import streamlit as st
 from langchain.storage import LocalFileStore
 from langchain_text_splitters import CharacterTextSplitter
@@ -12,6 +13,29 @@ st.set_page_config(
     page_icon="💬"
 )
 
+def embed_file (file):
+    file_content = file.read()
+    file_path=f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    cache_dir = LocalFileStore("./.cache/embeddings")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings) # we need to find another vectorstore service for further implementation. Chroma and FAISS is free vectorstore
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
 st.title("DocumentGPT")
 
 
@@ -24,27 +48,7 @@ st.markdown(
 
 file = st.file_uploader("Upload a .txt .pdf or .docx file", type=["pdf","txt","docx"],)
 
-if file:
-    st.write(file)
-    file_content = file.read()
-    file_path=f"./.cache/files/{file.name}"
-    st.write(file_content, file_path)
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-
-    cache_dir = LocalFileStore("./.cache/embeddings")
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=100,
-    )
-    loader = UnstructuredFileLoader("./files/allotment.txt")
-    docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
-        embeddings, cache_dir
-    )
-    vectorstore = FAISS.from_documents(docs, cached_embeddings) # we need to find another vectorstore service for further implementation. Chroma and FAISS is free vectorstore
-    retriever = vectorstore.as_retriever()
-    docs = retriever.index("allotment of shares")
-    st.write(docs)    
+if file:   
+    retriever = embed_file(file)
+    answer = retriever.invoke("Supporter")
+    answer   
