10930fad64e635a5a4ef483307fa88d9e6bcef17
bryanho0470 <bryanho0470@gmail.com>
2025-01-02 10:19:06 +0900

    Copy to streamlit
    - core function with notebook prompt and splitter, load, embeddings, vectorstore

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,5 +1,11 @@
 import streamlit as st
-import time
+from langchain.storage import LocalFileStore
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS, Chroma
+
+
 
 st.set_page_config(
     page_title="DocumentGPT",
@@ -26,5 +32,19 @@ if file:
     with open(file_path, "wb") as f:
         f.write(file_content)
 
-
-    
+    cache_dir = LocalFileStore("./.cache/embeddings")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader("./files/allotment.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings) # we need to find another vectorstore service for further implementation. Chroma and FAISS is free vectorstore
+    retriever = vectorstore.as_retriever()
+    docs = retriever.index("allotment of shares")
+    st.write(docs)    
