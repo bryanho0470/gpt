4ccc9874ab3738deba1436d86f213f956b0a082c
bryanho0470 <bryanho0470@gmail.com>
2025-01-02 09:40:17 +0900

    Streamlit
    - use session_state
    - use utility function
    
    how to store message and display history of conversation.

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -8,20 +8,27 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
-with st.chat_message("Human"):
-    st.write("Who are you?")
+if "messages" not in st.session_state:
+    # session_state is the dictionary
+    st.session_state["messages"] = []
 
-with st.chat_message("ai"):
-    st.write("how are you?")
+def send_message(message, role, save=True):
+    # we call send_message() UTILITY FUNCTION which defined by users.
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({"message":message, "role": role})
 
-st.chat_input("Send message to AI")
+for message in st.session_state["messages"]:
+    send_message(message["message"], message["role"], save=False)
 
-with st.status("Embedding Files....", expanded=True) as status:
-    time.sleep(3)
-    st.write("Getting the file")
-    time.sleep(3)
-    st.write("Embedding the file")
-    time.sleep(3)
-    st.write("Caching the file")
-    status.update(label="Error", state="error")
+
+message = st.chat_input("Send message to AI")
+
+#  if message mean there is something. but it doesnt work if you code "message !=""," but it is same with != None 
+if message != None: 
+    send_message(message, 'human')
+    time.sleep(2)
+    send_message(f"You said: {message}","ai")
+
     
