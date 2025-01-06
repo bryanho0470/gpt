d46790a2afd14a9d42913618b9c16323c07033f0
bryanho0470 <bryanho0470@gmail.com>
2025-01-06 20:20:49 +0900

    Local MOdel
    - GPT4ALL
    - Ollama

--- pages/02_PrivateGPT.py
+++ pages/02_PrivateGPT.py
@@ -67,11 +67,11 @@ def format_docs(docs):
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file (file):
     file_content = file.read()
-    file_path=f"./.cache/files/{file.name}"
+    file_path=f"./.cache/private_files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
 
-    cache_dir = LocalFileStore("./.cache/embeddings")
+    cache_dir = LocalFileStore("./.cache/private_embeddings")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
