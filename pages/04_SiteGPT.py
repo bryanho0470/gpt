49109adf3ced969c95e9ac7575c2de87e0100425
bryanho0470 <bryanho0470@gmail.com>
2025-02-08 14:41:04 +0900

    Html to text
    - install playwright - chromium
    - import document_transformer Html2TextTransformer

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,8 +1,36 @@
 import streamlit as st
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
 
 st.set_page_config(
     page_title="SiteGPT",
-    page_icon="💬"
+    page_icon="📺"
 )
 
+html2text_transformer = Html2TextTransformer()
+
 st.title("SiteGPT")
+
+st.markdown(
+    """
+    
+    #Site GPT
+    
+    Ask questions about the content of a website.
+
+    Start by writing the URL of the website on the sidebar.
+
+    """)
+
+with st.sidebar:
+        url = st.text_input(
+                "Write down a URL",
+                placeholder="https://example.com",
+        )
+
+if url:
+    loader = AsyncChromiumLoader([url])
+    docs = loader.load()
+    st.write(docs)
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(transformed)
