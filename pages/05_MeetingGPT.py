564b3b32c771c1bc30c6f85792e454de1cd53642
bryanho0470 <bryanho0470@gmail.com>
2025-02-12 14:30:43 +0900

    Refine chain
    - Create two chains
    - One is for create summary for the first document.
    - Second is for revise summary based on additional summary from other documents.

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,11 +1,20 @@
-from cProfile import label
 import streamlit as st
-import subprocess
-from pydub import AudioSegment
-import openai
-import math
-import glob
-import os
+import subprocess #to make a code sa same like CLI
+from pydub import AudioSegment #to make object can be search from audio file. 
+import openai #make transcript with Whisper from audio file
+import math # calculate 
+import glob #for searching specific file in the folder return object.
+import os #for control files
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import StrOutputParser
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
 
 has_transcript = os.path.exists("./files/transcripts/final_transcript.txt")
 
@@ -81,12 +90,56 @@ if video:
         status.update(label="All done! Thank you for your patients")
         st.write("🎉")
 
-transcript_tab, sumarry_tab, qna_tab = st.tabs(["Transcript","Summary","Q&A",])
+transcript_tab, summary_tab, qna_tab = st.tabs(["Transcript","Summary","Q&A",])
 
 with transcript_tab:
     with open(destination) as file:
         st.write(file.read())
 
+with summary_tab:
+    start = st.button("Generate Summary")
+    if start:
+        loader = TextLoader(destination)
+        splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=800, chunk_overlap=100,)
+        docs = loader.load_and_split(text_splitter=splitter)
+        st.write(docs)
+
+        first_summary_prompt = ChatPromptTemplate.from_template(
+        """
+        Write a concise summary of the following:
+        "{text}"
+        Concise summary:
+        """
+        )
+        first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+
+        summary = first_summary_chain.invoke({
+            "text":docs[0].page_content
+        })
+
+        refine_prompt = ChatPromptTemplate.from_template(
+            """
+            Your job is to produce a final summary.
+            We have provided an existing summary up to a certain point: {exsiting_summary}
+            We have the opportunity to refine the existing summary (only if needed) with some more context belo.
+            ----------
+            {context}
+            ----------
+            Given the new context, refine the original summary.
+            If the context isn't useful, RETURN the original summary.            
+            """
+        )
+
+        refine_chain = refine_prompt | llm | StrOutputParser()
+
+        with st.status("Summarizing......") as status:
+            for i, doc in enumerate(docs[1:]):
+                status.update(label=f"Processing Document {i}/{len(docs)-1}")
+                summary = refine_chain.invoke({
+                    "exsiting_summary": summary,
+                    "context": doc.page_content
+                })
+        st.write(summary)
 
 
 
