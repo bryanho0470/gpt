8ae7d4ed244e58d344572a8c210e4e78ec946f1d
bryanho0470 <bryanho0470@gmail.com>
2025-03-15 15:58:49 +0900

    create custom GPT for Chef

--- main2.py
+++ main2.py
@@ -1,35 +1,68 @@
-import stat
-from fastapi import Body, Form, FastAPI, Request
+from fastapi import Form, FastAPI
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, Field
-from typing import Dict, Any
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+import getpass
+import time
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from uuid import uuid4
 
-class Quote(BaseModel):
-    quote : str = Field(description="The quote that Nicolacus Maximus said")
-    year : int = Field(description="the year when Nicolacus Maximus said the quote")
+from pinecone import Pinecone, ServerlessSpec
+
+load_dotenv()
+
+if not os.getenv("PINECONE_API_KEY"):
+    os.environ["PINECONE_API_KEY"] = getpass.getpass("Enter your Pinecone API key: ")
+
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+pc = Pinecone(api_key=pinecone_api_key)
+index_name = "recipes"
+
+existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+
+if index_name not in existing_indexes:
+    pc.create_index(
+        name=index_name,
+        dimension=1536,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+    while not pc.describe_index(index_name).status["ready"]:
+        time.sleep(1)
+index = pc.Index(index_name)
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small") 
+
+vector_store = PineconeVectorStore.from_existing_index(
+    
+    embedding=embeddings,
+    index_name=index_name,
+)
 
 app = FastAPI(
-    title= " NICOLOACUS MAXIMUS QUOTE GIVER",
-    description="Get a real quote said by Nicolacus Maximus himself",
+    title= "ChefGPT. The best provider of Indian Recipes in the world.",
+    description="Give ChefGPT a couple of ingredients and it will give you recipes in return.",
     servers=[
-        {"url" : "https://vaccine-concepts-such-penguin.trycloudflare.com"}
+        {"url" : "https://networking-prostate-steam-conferences.trycloudflare.com"}
     ]
 )
 
-@app.get("/quote", 
-         summary="Returns a random quote by Nicolacus Maximus", 
-         description="Upon receiving a GET request this endpoint will return a real quote said by Nicolacus Maximus himself.", 
-         response_description="A quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said",
-         response_model=Quote,
+class Document(BaseModel):
+    page_content: str
+
+@app.get("/recipes", 
+         summary="Return a list of recipes.", 
+         description="Upon receiving an ingredient, this endpoint will return a list of recipes that contain that ingredient", 
+         response_description="A document object that contains the recipe and preparetion instructions",
+         response_model=list[Document],
          openapi_extra={
-             "x-openai-isConsequential":True,}
+             "x-openai-isConsequential":False,}
          )
 
-
-def get_quote(request : Request):
-    print(request.headers)
-    return {"quote":"Life is short. so eat it all","year":2025}
-
+def get_recipes(ingredient: str):
+    search = vector_store.similarity_search(ingredient)
+    return search
 
 user_token_db = { "ABCDEF": "Park"}
 
